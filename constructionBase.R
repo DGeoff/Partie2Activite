51b0865d2925b0584c17d37303e0a281497e85a9
GeoffroyD <geoffroy.detrousselle@live.fr>
2017-09-13 22:33:35 +0200

    Modification du fichier constructionBase.R

---
+++ constructionBase.R
@@ -0,0 +1,4 @@
+# construction du jeu de données 
+tableau <- data.frame(Voiture = c("Renault Megane","Peugeot 307","Citroen C3","Citroen C3","Citroen C3","Peugeot 307","Peugeot 307","Renault Megane","Peugeot 307","Peugeot 307","Renault Megane","Peugeot 307","Citroen C3","Citroen C3","Renault Megane","Citroen C3","Citroen C3","Renault Megane","Renault Megane","Renault Megane","Citroen C3")
+					,Annee = c("2010","2010","2010","2010","2012","2013","2013","2017","2016","2015","2016","2012","2010","2014","2017","2014","2010","2011","2017","2016","2011")
+					,Prix = c(4500,3000,4550,6000,7892,2500,6520,5800,6800,6000,6300,5900,4000,3500,6200,4800,6800,7100,5600,4200,2900))
